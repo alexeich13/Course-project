0dd416774f37dbb66698d4ff1bc60664aef78f6d
Alexey <96183855+alexeich13@users.noreply.github.com>
2022-05-19 03:38:16 +0300

    Add files via upload

---
+++ JS/Cars.js
@@ -0,0 +1,68 @@
+const CARS = [
+  {
+    modal: "Porsche 718 Cayman ",
+    img: "https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_73029c096b844d559e5e955567f42699.jpg",
+    characteristic:
+      "Модели 718 созданы для жизни в спортивном стиле. Это среднемоторные спортивные автомобили, которые хранят в себе спортивный дух легендарного Porsche 718",
+    power: "220 кВт/300 л.с.",
+    speed: "275 км/ч",
+    overclocking: "5,1 с",
+    cylinders: "4",
+    price_number: "67 000",
+  },
+  {
+    modal: "Porsche 911 Turbo",
+    img: "https://pc-rk.ru/wp-content/uploads/2017/06/5938f0e0ec05c41a5300000e.jpg",
+    characteristic:
+      "Наши инженеры стремятся создавать совершенные, но при этом комфортабельные и практичные спорткары. Модели 911 Turbo полностью соответствуют этому подходу.",
+    power: "427 кВт/580 л.с.",
+    speed: "320 км/ч",
+    overclocking: "2,8 с",
+    cylinders: "6",
+    price_number: "67 000",
+  },
+  {
+    modal: "Porsche Taycan",
+    img: "https://www.domkrat.by/upload/img_catalog/taycan/Porsche_Taycan_5.jpg",
+    characteristic:
+      "Лучшие спортивные моменты — это секунды после состояния эйфории бегуна, когда наша душа находится в идеальном балансе. С новыми моделями Taycan Sport Turismo это чувство можно испытывать каждый день. ",
+    power: "300 кВт/408 л.с.",
+    speed: "230 км/ч",
+    overclocking: "5,4 с",
+    cylinders: "4",
+    price_number: "84 000",
+  },
+  {
+    modal: "Porsche Panamera",
+    img: "https://motor.ru/imgs/2021/11/09/11/5013613/d6760fc27fdb397127f186d9b148ff9e72e28e55.jpeg",
+    characteristic:
+      "Спортивный автомобиль для четверых? С впечатляющей динамикой и большим комфортом? С типичной для Porsche динамикой и одновременно высокой экономичностью? Невозможно – говорили многие. Смело – утверждали другие.",
+    power: "243 кВт/330 л.с.",
+    speed: "270 км/ч",
+    overclocking: "5,6 с",
+    cylinders: "6",
+    price_number: "103 000",
+  },
+  {
+    modal: "Porsche Macan",
+    img: "https://kolesa-uploads.ru/-/29cec33f-9344-4268-8c16-315214fd51e4/0-0.jpg",
+    characteristic:
+      "Выделяйтесь там, где другие растворяются в массах: новый Macan T. Молодой, динамичный и городской.",
+    power: "195 кВт/265 л.с.",
+    speed: "232 км/ч",
+    overclocking: "6,4 с",
+    cylinders: "4",
+    price_number: "65 000",
+  },
+  {
+    modal: "Porsche Cayenne",
+    img: "https://carexpert.ru/img/foto600/porsche/porsc191.jpg",
+    characteristic:
+      "Линейка Cayenne пополнилась еще одним представителем, обладающим невероятной мощью и точной динамикой.",
+    power: "250 кВт/340 л.с.",
+    speed: "245 км/ч",
+    overclocking: "6,2 с",
+    cylinders: "6",
+    price_number: "88 000",
+  },
+];
